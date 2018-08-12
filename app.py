3db42c236828cbc2b32f00f567e662a7706ee274
Amulya <amulya.mummaneni@gmail.com>
2018-08-11 21:42:05 -0400

    Added profile pagegit statusgit status + some styling

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
-profilePage = [[]]
 
 @app.route('/', methods = ["GET"]) 
 def index():
@@ -102,11 +101,20 @@ def prefs():
 	error = []
 	profilePage = [[]]
 	#tech=None
+
 	if 'username' not in session:
 		return render_template('index.html')
 	username_session = escape(session['username']).capitalize()
 	username = escape(session['username'])
-	
+
+	# Rendering profile page (if form was already filled out)
+	cur = mysql.connection.cursor()
+	cur.execute("SELECT userID FROM user WHERE username=%s", [username])
+	userID = cur.fetchone()[0]
+	cur.execute("SELECT hackathonID FROM usertohackathon WHERE userID=%s", [userID])
+	if cur.fetchone()[0] is not None:
+		return redirect(url_for('profile'))
+
 	if request.method == 'POST':
 		
 		# simple fields 
@@ -124,86 +132,54 @@ def prefs():
 		ints = request.form.getlist('interests[]')
 		hardware = request.form.getlist('hardware[]')
 
-		profilePage[0] = [hackathon_form]
-		profilePage[1] = [projIdea_form]
-		profilePage[2] = tech
-		profilePage[3] = interests
-		profilePage[4] = languages
-		profilePage[5] = hardware
-		profilePage[6] = [exper]
-		profilePage[7] = [compLevel_form]
-		profilePage[8] = [gitLink_form]
-		profilePage[9] = [resume_form]
-
+		
 		cur = mysql.connection.cursor()
 		cur.execute("SELECT userID FROM user WHERE username=%s", [username])
 		userID = cur.fetchone()[0]
 
 		# hackathon
-		if hackathon_form is None:
-			error.append('Please select a hackathon.')
-			isError = True
-		else:
-			cur.execute("SELECT hackathonID FROM hackathons WHERE hackathon=%s", [hackathon_form])
-			hackathonID = cur.fetchone()[0]
-			cur.execute("INSERT INTO usertohackathon VALUES(%s, %s)", [userID, hackathonID])
+		cur.execute("SELECT hackathonID FROM hackathons WHERE hackathon=%s", [hackathon_form])
+		hackathonID = cur.fetchone()[0]
+		cur.execute("INSERT INTO usertohackathon VALUES(%s, %s)", [userID, hackathonID])
 
 		# multiselect fields: cycle through, add each one as a row in many-to-many table
 
 		#technologies 
-		if len(tech) == 0:
-			isError = True
-			error.append("Please select at least one technology.")
-		else:
+		if len(tech) != 0:
 			for i in range(0, len(tech)):
 				cur.execute("SELECT techID FROM tech WHERE tech=%s", [tech[i]])
 				techID = cur.fetchone()[0]
 				cur.execute("INSERT INTO usertotech VALUES(%s, %s)", [userID, techID])
 
 		# languages 
-		if len(languages) == 0:
-			isError = True
-			error.append("Please select at least one language.")
-		else:
+		if len(languages) != 0:
 			for i in range(0, len(languages)):
 				cur.execute("SELECT langID FROM langs WHERE lang=%s", [languages[i]])
 				langID = cur.fetchone()[0]
 				cur.execute("INSERT INTO usertolang VALUES(%s, %s)", [userID, langID])
 
 		# interests 
-		if len(ints) == 0:
-			isError = True
-			error.append("Please select at least one interest.")
-		else:
+		if len(ints) != 0:
 			for i in range(0, len(ints)):
 				cur.execute("SELECT intID FROM interests WHERE interest=%s", [ints[i]])
 				intID = cur.fetchone()[0]
 				cur.execute("INSERT INTO usertointerests VALUES(%s, %s)", [userID, intID])
 
-
 		# hardware 
-		if len(hardware) == 0:
-			isError = True
-			error.append("Please select at least one hardware.")
-		else:
+		if len(hardware) != 0:
 			for i in range(0, len(hardware)):
 				cur.execute("SELECT hwID FROM hw WHERE hw=%s", [hardware[i]])
 				hwID = cur.fetchone()[0]
 				cur.execute("INSERT INTO usertohw VALUES(%s, %s)", [userID, hwID])
 
+
 		# radio options
-		if len(experLevel_form) == 0:
-			error.append('Please select an experience level.')
-			isError = True
-		else:
+			# Why update statements???????
+		if len(exper_form) != 0:
 			cur.execute("UPDATE user SET exper=%s WHERE username=%s", [exper_form, username]) 
 			mysql.connection.commit()
 
-
-		if len(compLevel_form) == 0:
-			error.append('Please select a competition level.')
-			isError = True
-		else:
+		if len(compLevel_form) != 0:
 			cur.execute("UPDATE user SET comp=%s WHERE username=%s", [compLevel_form, username]) 
 			mysql.connection.commit()
 
@@ -232,15 +208,106 @@ def prefs():
 		if isError: 
 			render_template('prefs.html', error = error)
 
-		flash('Thanks for filling out your profile! Click here to find your matches.')
-		return render_template('profile.html', username=username_session, profilePage=profilePage)
+		# fix this message?
+		flash('Thanks for filling out your profile! Scroll down to explore your matches.')
+		
+		return redirect(url_for('profile'))
 	return render_template('prefs.html', username=username_session)
 
 @app.route('/profile')
 def profile():
-	if len(profilePage) > 0:
-		render_template(url_for('profile', profilePage=profilePage))
-	render_template(url_for('profile'))
+	cur = mysql.connection.cursor()
+	username = escape(session['username'])
+
+	# use select statements, send all vars to template
+
+	cur.execute("SELECT userID FROM user WHERE username=%s", [username])
+	userID = cur.fetchone()[0] # need for many-to-many relationships
+
+	# hackathon
+	cur.execute("SELECT hackathonID FROM usertohackathon WHERE userID=%s", [userID])
+	hID = cur.fetchone()[0]
+	cur.execute("SELECT hackathon FROM hackathons WHERE hackathonID=%s", [hID])
+	hackathon = cur.fetchone()[0]
+
+	# project idea
+	cur.execute("SELECT projIdea FROM user WHERE username=%s", [username])
+	projIdea = cur.fetchone()[0]
+
+	# multiselect items: have access to IDs. must select ID, then use it to find name of item in its own table
+
+	# technologies
+	cur.execute("SELECT * FROM usertotech WHERE userID=%s", [userID])
+	techList = []
+	for row in cur.fetchall(): # loop through rows in usertotech
+		techID = row[1]
+		cur.execute("SELECT tech FROM tech WHERE techID=%s", [techID])
+		tech = cur.fetchone()[0]
+		techList.append(tech)
+	if len(techList) == 0:
+		techList = None
+
+	# interests
+	cur.execute("SELECT * FROM usertointerests WHERE userID=%s", [userID])
+	intList = []
+	for row in cur.fetchall(): # loop through rows in usertotech
+		intID = row[1]
+		cur.execute("SELECT interest FROM interests WHERE intID=%s", [intID])
+		interest = cur.fetchone()[0]
+		intList.append(interest)
+	if len(intList) == 0:
+		intList = None
+
+	# languages
+	cur.execute("SELECT * FROM usertolang WHERE userID=%s", [userID])
+	langList = []
+	for row in cur.fetchall(): # loop through rows in usertotech
+		langID = row[1]
+		cur.execute("SELECT lang FROM langs WHERE langID=%s", [langID])
+		lang = cur.fetchone()[0]
+		langList.append(lang)
+	if len(langList) == 0:
+		langList = None
+
+	# hardware
+	cur.execute("SELECT * FROM usertohw WHERE userID=%s", [userID])
+	hwList = []
+	for row in cur.fetchall(): # loop through rows in usertotech
+		hwID = row[1]
+		cur.execute("SELECT hw FROM hw WHERE hwID=%s", [hwID])
+		hw = cur.fetchone()[0]
+		hwList.append(hw)
+	if len(hwList) == 0:
+		hwList = None
+
+	# experience level
+	cur.execute("SELECT exper FROM user WHERE username=%s", [username])
+	exper = cur.fetchone()[0]
+
+	# competition level
+	cur.execute("SELECT comp FROM user WHERE username=%s", [username])
+	comp = cur.fetchone()[0]
+
+	# github link
+	cur.execute("SELECT gitLink FROM user WHERE username=%s", [username])
+	gitLink = cur.fetchone()[0]
+
+	# resume link
+	cur.execute("SELECT resume FROM user WHERE username=%s", [username])
+	resume = cur.fetchone()[0]
+
+	return render_template('profile.html', username=username.capitalize(), hackathon=hackathon, projIdea=projIdea, 
+		techList=techList, intList=intList, langList=langList, hwList=hwList, exper=exper, comp=comp, gitLink=gitLink, resume=resume)
+	
+@app.route('/update')
+def update():
+	username = escape(session['username'])
+	return render_template('update.html', username=username)
+	
+@app.route('/matches')
+def matches():
+	username = escape(session['username'])
+	return render_template('matches.html', username=username)
 	
 app.secret_key = 'MVB79L'
 
